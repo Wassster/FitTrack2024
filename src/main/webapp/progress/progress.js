c8138e870cd3122fcc25d0cc263e51e2a9bf7a46
wassim <wassimzenasi@gmail.com>
2024-06-30 17:13:40 +0200

    progress grafiek gemaakt even kijken hoe ik dit zou moeten implementeren en style goed maken

--- src/main/webapp/progress/progress.js
+++ src/main/webapp/progress/progress.js
@@ -1,6 +1,51 @@
-var ctx = document.getElementById("chart")
-var chart = new Chart(ctx,{
+const ctx = document.getElementById('myChart');
 
-    type : 'line',data:{},options:{}
 
-} );
+fetch("/api/workout", {
+    method: "GET",
+    headers: {
+        "Authorization": `Bearer ${window.sessionStorage.getItem("myJWT")}`
+    }
+})
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return response.text().then(text => {
+                throw new Error(text);
+            });
+        }
+    })
+    .then(data => {
+        console.log(data)
+        newChart(data,)
+    })
+    .catch(error => {
+        console.error("Error fetching profile:", error);
+    });
+
+
+function newChart(data){
+    data.forEach(workout => {
+        const exerciseNames = workout.exercises.map(exercise => exercise.name);
+        const exerciseWeight = workout.exercises.map(exercise => exercise.weight);
+
+    new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: exerciseNames,
+            datasets: [{
+                label: 'weight',
+                data: exerciseWeight,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });})}
+
